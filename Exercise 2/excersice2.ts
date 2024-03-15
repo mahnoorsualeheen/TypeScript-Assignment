2706d33e5cb2b18c5a4f8f6d3f5e8a57af629cb0
Mahnoor Malik <160276957+mahnoorsualeheen@users.noreply.github.com>
2024-03-15 09:17:33 +0500

    Add files via upload

---
+++ Exercise 2/excersice2.ts
@@ -0,0 +1,9 @@
+/*Excersice 2: Personal Message: Store a person’s name in a variable, and print a message to that person.
+ Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+
+
+let personName: string = "Mahnoor"
+
+let message: string = `"Hello ${personName}, would you like to be a programmer?"`
+
+console.log(message)
