e3056ab7d7866c4b706138a42d1d9740428197a8
Mahnoor Malik <160276957+mahnoorsualeheen@users.noreply.github.com>
2024-03-16 07:35:40 +0500

    Add files via upload

---
+++ Excersice 6/excersice6.js
@@ -0,0 +1,5 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+var personName = "\t Mahnoor Malik \n";
+console.log("Name with White Space:", personName);
+var NameAfterStrip = personName.trim();
+console.log("Striped Name:", NameAfterStrip);
